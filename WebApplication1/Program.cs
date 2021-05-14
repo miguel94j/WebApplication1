25d86475584225865442f7d6181822eaf01bfa81
miguel123j <84193557+miguel123j@users.noreply.github.com>
2021-05-14 12:03:19 -0500

    segundoconflic

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -21,12 +21,15 @@ namespace WebApplication1
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+        /// <summary>
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-
+                    //conflicto mayor 3
                     //TAMBIEN QUIERO CREAR CONFLICTOS
                     webBuilder.UseStartup<Startup>();
                     webBuilder.UseStartup<Startup>();
